e724574da0a6617b9ceec8c85f1679216f588be3
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-09-18 14:39:19 +0100

    Create constant Values for Db Table fields

---
+++ app/src/main/java/com/spykins/todo/constant/DbConstant.java
@@ -0,0 +1,15 @@
+package com.spykins.todo.constant;
+
+/**
+ * Created by Spykins on 18/09/16.
+ */
+public class DbConstant {
+    public static final String DB_NAME = "Todo_Db";
+    public static final String TODO_COLUMN_ID = "Todo_id";
+    public static final String TODO_COLUMN_TODO_TITLE = "Todo_name";
+    public static final String TODO_COLUMN_TODO_DESCRIPTION = "Todo_description";
+    public static final String TODO_COLUMN_TODO_TIME = "Todo_time";
+    public static final String TODO_COLUMN_ALERT_TIME_BEFORE = "Todo_alert_before";
+    public static final String TODO_TABLE = "Todo_table";
+    public static final String TODO_COLUMN_HAS_SHOWN = "Todo_hasShown";
+}
