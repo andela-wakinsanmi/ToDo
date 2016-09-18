5a6901a2def80e65d7c1dde5a0548488d4437cbf
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-09-18 16:08:08 +0100

    SetUp the View for Adding Todo in the App

--- app/src/main/java/com/spykins/todo/AddNewTodoActivity.java
+++ app/src/main/java/com/spykins/todo/AddNewTodoActivity.java
@@ -16,14 +16,12 @@ public class AddNewTodoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+
+    private void displaySnackBar() {
+        Snackbar.make(findViewById(R.id.rootView), "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 
 }
