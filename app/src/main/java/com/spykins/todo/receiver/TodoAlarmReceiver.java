5e620828ab66df4f8381dde7e983a3fd15e86740
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-09-18 15:38:10 +0100

    Set Up receiver for Notification and Alarm Manager

---
+++ app/src/main/java/com/spykins/todo/receiver/TodoAlarmReceiver.java
@@ -0,0 +1,16 @@
+package com.spykins.todo.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Spykins on 18/09/16.
+ */
+public class TodoAlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+}
