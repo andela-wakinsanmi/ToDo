0e50be22d94fee3d2d957f63873cc81e14f5dca2
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-09-18 16:26:51 +0100

    Implement the AddNewTodoInterface in the AddNewTodoActivity and also instantiate the presenter to the view

--- app/src/main/java/com/spykins/todo/AddNewTodoActivity.java
+++ app/src/main/java/com/spykins/todo/AddNewTodoActivity.java
@@ -7,21 +7,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class AddNewTodoActivity extends AppCompatActivity {
+import com.spykins.todo.interactor.AddTodoPresenter;
+import com.spykins.todo.view_interface.AddNewTodoInterface;
 
+public class AddNewTodoActivity extends AppCompatActivity implements AddNewTodoInterface {
+    private AddTodoPresenter addTodoPresenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dd_new_todo);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        addTodoPresenter = new AddTodoPresenter(this);
         setSupportActionBar(toolbar);
 
     }
 
-
-    private void displaySnackBar() {
-        Snackbar.make(findViewById(R.id.rootView), "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+    @Override
+    public void successfullyCreated() {
+        finish();
     }
 
+    @Override
+    public void reportError(String error) {
+        displaySnackBar(error);
+    }
+
+    private void displaySnackBar(String textToDisplay) {
+        Snackbar.make(findViewById(R.id.rootView), textToDisplay, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
 }
