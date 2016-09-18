4e3c98cf2e66d98baaf7abaf850a6b690a62a53b
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-09-18 15:20:18 +0100

    Add dependencies for test and enabled Jack Options

---
+++ app/src/androidTest/java/com/spykins/todo/db/DbHandlerTest.java
@@ -0,0 +1,14 @@
+package com.spykins.todo.db;
+
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Spykins on 18/09/16.
+ */
+@RunWith(AndroidJUnit4.class)
+public class DbHandlerTest {
+
+
+}
